93e7c80dea3fa55054020dac68e4c26e4148a0ea
Hashir Mahmood <166294183+Hashir-Mahmood-704@users.noreply.github.com>
2024-10-30 02:20:30 +0500

    learning strings

---
+++ strings.py
@@ -0,0 +1,15 @@
+myString = "my name is python"
+
+# """ """, 3 quotes me string hm tb likhtay hein jb hmari string lmbi ho or multiple lines me aati ho
+myLongString = """I am coding in python, right now.
+This is a new line.
+I use vscode for coding."""
+
+# string indexing
+print(myString[0])
+print(myString[2])
+print(myString[-1])
+# : colon syntax hm tb use krtay jb hmay index base pr multiple charaters chahye hotay, e.g printing characters from index 1 till index 8 (last index include nhi hota)
+print(myString[1: 9])
+# index 0 say 14 tk characters print ho rhay lekin 1 element k skip k sath
+print(myString[0: 15: 2])
